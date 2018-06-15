6a64fe9884d347396642c9a4b421ff60be2b8fee
Mary Kouvela <marykouvela@gmail.com>
2018-06-15 09:13:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const bluebird = require('bluebird');
+
+mongoose.connect('mongodb://localhost:27017/txt-data-db');
+
+const Data = require('./models/data');
+
+const filename = './arxiv-test-data.txt';
+
+function readTxtFile(filename) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(filename, 'utf8', (err, data) => {
+			if (err) {
+				return reject(err);
+			}
+			return resolve(data);
+		});
+	});
+}
+
+function dataToArray(data, err) {
+	return new Promise((resolve, reject) => {
+		if (err) {
+			return reject(err);
+		}
+		const array = data.toString().split('\n');
+		return resolve(array);
+	});
+}
+
+function saveItems(items) {
+	return bluebird.map(items, item => {
+		return saveItem(item);
+	});
+}
+
+function saveItem(item) {
+	const parsedItem = JSON.parse(item);
+	return Data.create(parsedItem)
+		.catch(err => {
+			// console.log(err);
+		});
+}
+
+function getItemsFromDB() {
+	const start = new Date();
+	return Data.find({keywords: {$in: ['Differential Geometry']}}).then(items => {
+		console.log('Elapsed', new Date() - start);
+		return items;
+	});
+}
+
+function addSerialNumber(items) {
+	let cnt = 0;
+	return bluebird.map(items, item => {
+		item.serialNumber = ++cnt;
+		return saveItemToFile(item);
+	});
+}
+
+function saveItemToFile(item) {
+	return new Promise((resolve, reject) => {
+		item = JSON.stringify(item);
+		fs.appendFile('./new-file.txt', item, err => {
+			if (err) {
+				return reject(err);
+			}
+			return resolve('Ok');
+		});
+	});
+}
+
+readTxtFile(filename)
+	.then(data => dataToArray(data))
+	.then(items => saveItems(items))
+	.then(() => getItemsFromDB())
+	.then(items => addSerialNumber(items));
+
---
+++ models/data.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+	abstract: {type: String},
+	doi: {type: String, unique: true},
+	title: {type: String},
+	createdAt: {type: Date},
+	keywords: {type: Array, index: true}
+});
+
+module.exports = mongoose.model('Data', dataSchema);
