2f38962c026311a1da70ea76547b0e760d08f124
Napoleon Oikonomou <Napoleonoikon@gmail.com>
2019-10-28 11:05:48 +0200

    Add eslint, Meta tweaks

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const bluebird = require('bluebird');
+const fs = require("fs");
+const mongoose = require("mongoose");
+const bluebird = require("bluebird");
 
-mongoose.connect('mongodb://localhost:27017/txt-data-db');
+mongoose.connect("mongodb://localhost:27017/txt-data-db");
 
-const Data = require('./models/data');
+const Data = require("./models/data");
 
-const filename = './arxiv-test-data.txt';
+const filename = "./arxiv-test-data.txt";
 
-function readTxtFile(filename) {
+function readTxtFile(filePath) {
 	return new Promise((resolve, reject) => {
-		fs.readFile(filename, 'utf8', (err, data) => {
+		fs.readFile(filePath, "utf8", (err, data) => {
 			if (err) {
 				return reject(err);
 			}
@@ -24,37 +24,34 @@ function dataToArray(data, err) {
 		if (err) {
 			return reject(err);
 		}
-		const array = data.toString().split('\n');
+		const array = data.toString().split("\n");
 		return resolve(array);
 	});
 }
 
 function saveItems(items) {
-	return bluebird.map(items, item => {
-		return saveItem(item);
-	});
+	return bluebird.map(items, (item) => saveItem(item));
 }
 
 function saveItem(item) {
 	const parsedItem = JSON.parse(item);
 	return Data.create(parsedItem)
-		.catch(err => {
-			// console.log(err);
-		});
+		.catch(() => {});
 }
 
 function getItemsFromDB() {
 	const start = new Date();
-	return Data.find({keywords: {$in: ['Differential Geometry']}}).then(items => {
-		console.log('Elapsed', new Date() - start);
+	return Data.find({ keywords: { $in: ["Differential Geometry"] } }).then((items) => {
+		console.log("Elapsed", new Date() - start);
 		return items;
 	});
 }
 
 function addSerialNumber(items) {
 	let cnt = 0;
-	return bluebird.map(items, item => {
-		item.serialNumber = ++cnt;
+	return bluebird.map(items, (item) => {
+		cnt += 1;
+		item.serialNumber = cnt;
 		return saveItemToFile(item);
 	});
 }
@@ -62,18 +59,17 @@ function addSerialNumber(items) {
 function saveItemToFile(item) {
 	return new Promise((resolve, reject) => {
 		item = JSON.stringify(item);
-		fs.appendFile('./new-file.txt', item, err => {
+		fs.appendFile("./new-file.txt", item, (err) => {
 			if (err) {
 				return reject(err);
 			}
-			return resolve('Ok');
+			return resolve("Ok");
 		});
 	});
 }
 
 readTxtFile(filename)
-	.then(data => dataToArray(data))
-	.then(items => saveItems(items))
+	.then((data) => dataToArray(data))
+	.then((items) => saveItems(items))
 	.then(() => getItemsFromDB())
-	.then(items => addSerialNumber(items));
-
+	.then((items) => addSerialNumber(items));
--- models/data.js
+++ models/data.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const dataSchema = new mongoose.Schema({
-	abstract: {type: String},
-	doi: {type: String, unique: true},
-	title: {type: String},
-	createdAt: {type: Date},
-	keywords: {type: Array, index: true}
+	abstract: { type: String },
+	doi: { type: String, unique: true },
+	title: { type: String },
+	createdAt: { type: Date },
+	keywords: { type: Array, index: true },
 });
 
-module.exports = mongoose.model('Data', dataSchema);
+module.exports = mongoose.model("Data", dataSchema);
